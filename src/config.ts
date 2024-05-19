0d1360f6ffe7dd9e783f37b4a31e9367118ed0f2
WholeFoodGuy <admin@wholefoodguy.com>
2024-05-18 21:16:49 -0400

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -50,9 +50,9 @@ export const SOCIALS: SocialObjects = [
   },
   {
     name: "Mail",
-    href: "mailto:yourmail@gmail.com",
+    href: "mailto:feedback75923@wholefoodguy.com",
     linkTitle: `Send an email to ${SITE.title}`,
-    active: false,
+    active: true,
   },
   {
     name: "Twitter",
